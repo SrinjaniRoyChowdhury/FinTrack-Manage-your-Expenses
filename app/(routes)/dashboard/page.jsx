a199585ffa5069db529232ac8fcef3b363e3db4e
Srinjani Roy Chowdhury <roysrinjani04@gmail.com>
2026-04-18 16:53:42 +0530

    feat: basic dashboard layout along with sidenav

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,10 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
+import SideNav from "./_components/SideNav";
 
 function Dashboard() {
     return (
         <div>
-            <UserButton />
+            dashboard
         </div>
     )
 }
